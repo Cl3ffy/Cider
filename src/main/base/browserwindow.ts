18daaf5f7b9b1c2cb95ec7718facaa9bdb9a43f4
vapormusic <vietanhfat@gmail.com>
2022-06-26 07:49:46 +0700

    some fixes

--- src/main/base/browserwindow.ts
+++ src/main/base/browserwindow.ts
@@ -1255,7 +1255,7 @@ export class BrowserWindow {
                                             "assetUrl":  "file:///" +audio,
                                             "contentAdvisory": "",
                                             "releaseDateTime": "2022-05-13T00:23:00Z",
-                                            "durationInMilliseconds": Math.floor((metadata.format.duration?? 0) * 1000),
+                                            "durationInMillis": Math.floor((metadata.format.duration?? 0) * 1000),
                                             
                                             "offers": [
                                                 {
